cd0f288a99fddc8cb2afb566f17e98068d04703a
jraduege <git@lvec.org>
2022-03-19 17:13:42 -0700

    Add computerPlay() function

---
+++ rps.js
@@ -0,0 +1,16 @@
+function computerPlay() {
+    let num = Math.floor(Math.random()*3)+1
+    switch (num) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+    }
+}
+
+console.log(computerPlay());
