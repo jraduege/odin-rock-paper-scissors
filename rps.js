378daeae0e9f7bf42eaa861dd953eaf6b7efb64b
jraduege <git@lvec.org>
2022-03-19 21:24:31 -0700

    Add player selection.  Fix cases

--- rps.js
+++ rps.js
@@ -2,73 +2,99 @@ function computerPlay() {
     let num = Math.floor(Math.random()*3)+1
     switch (num) {
         case 1:
-            return "Rock";
+            return "rock";
             break;
         case 2:
-            return "Paper";
+            return "paper";
             break;
         case 3:
-            return "Scissors";
+            return "scissors";
             break;
     }
 }
 
 function vsRock(selection) {
     switch (selection) {
-        case "Rock":
-            return "Tie: you both chose Rock"
+        case "rock":
+            return "Tie: you both chose rock"
             break;
-        case "Paper":
-            return "You win!: Paper covers Rock"
+        case "paper":
+            return "You win!: paper covers rock"
             break;
-        case "Scissors":
-            return "You lose: Rock breaks Scissors"
+        case "scissors":
+            return "You lose: rock breaks scissors"
             break;
     }
 }
 
 function vsPaper(selection) {
     switch (selection) {
-        case "Paper":
-            return "Tie: you both chose Paper"
+        case "paper":
+            return "Tie: you both chose paper"
             break;
-        case "Scissors":
-            return "You win!: Scissors cuts Paper"
+        case "scissors":
+            return "You win!: scissors cuts paper"
             break;
-        case "Rock":
-            return "You lose: Paper covers Rock"
+        case "rock":
+            return "You lose: paper covers rock"
             break;
     }
 }
 
 function vsScissors(selection) {
     switch (selection) {
-        case "Scissors":
-            return "Tie: you both chose Scissors"
+        case "scissors":
+            return "Tie: you both chose scissors"
             break;
-        case "Rock":
-            return "You win!: Rock breaks Scissors"
+        case "rock":
+            return "You win!: rock breaks scissors"
             break;
-        case "Paper":
-            return "You lose: Scissors cuts Paper"
+        case "paper":
+            return "You lose: scissors cuts paper"
             break;
     }
 }
 
 function playRound(playerSelection, computerSelection) {
     switch (computerSelection) {
-        case "Rock":
+        case "rock":
             return vsRock(playerSelection);
             break;
-        case "Paper":
+        case "paper":
             return vsPaper(playerSelection);
             break;
-        case "Scissors":
+        case "scissors":
             return vsScissors(playerSelection);
             break;
     }
 }
 
+function getPlayerSelection() {
+    let inputValue=prompt("Choose rock, paper, or scissors");
+    let lowerValue=inputValue.toLowerCase();
+    switch (lowerValue) {
+        case "r":
+            return "rock";
+            break;
+        case "p":
+            return "paper";
+            break;
+        case "s":
+            return "scissors";
+            break;
+    }
+    while (!((lowerValue==="rock")||(lowerValue==="paper")||(lowerValue==="scissors"))) { 
+        let inputValue=prompt("That is not a valid choice.  Choose rock, paper, or scissors:");
+        let lowerValue=inputValue.toLowerCase();
+    }
+    return lowerValue;
+}
+
+function game(rounds) {
+    for (let i = 0; i < rounds; i++) {
+        let playerSelection = getPlayerSelection();
+        let outcome=playRound(playerSelection, computerPlay());
+}
 const playerSelection = "Rock";
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
